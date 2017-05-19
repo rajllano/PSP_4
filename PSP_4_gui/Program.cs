96ccebbfd089a072bd63a543c3511f647e8d4b3e
Max <Max@DESKTOP>
2017-05-19 00:56:51 -0500

    Unas mejoras en la interfaz de usuario

--- PSP_4_gui/Program.cs
+++ PSP_4_gui/Program.cs
@@ -11,7 +11,23 @@ namespace PSP_4_gui
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("************************************************");
+            Console.WriteLine("*** PSP4 - Regla de Simpson                  ***");
+            Console.WriteLine("************************************************");
+            Console.WriteLine("Para procesar un conjunto de valores x1, x2 y dof");
+            Console.WriteLine("se debe crear un archivo de texto con la extención .txt");
+            Console.WriteLine(@"y guardarlos en el directorio C:\PSP4");
+            Console.WriteLine(@"La estructura del archivo debe ser:");
+            Console.WriteLine(@"ValorX1;ValorX2;dof");
+            Console.WriteLine(@"Ejemplo:");
+            Console.WriteLine(@"0;1,1;9");
+            Console.WriteLine(@"0;1,1812;10");
+            Console.WriteLine(@"0;2,750;30");
+            Console.WriteLine("---Presione una tecla para iniciar el procesamiento---");
+            Console.ReadKey();
+
             ControlArchivo.ProcesarArchivo(@"C:\PSP4\Datos.txt");
+            Console.WriteLine("---Presione una tecla para salir---");
             Console.ReadKey();
         }
     }
